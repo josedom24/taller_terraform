3ed5e380fb910fa5a9192e2f2f5e088041c08c0d
José Domingo Muñoz <josedom24@gmail.com>
2022-02-20 20:26:59 +0100

    terraform

--- ejemplo2/main.tf
+++ ejemplo2/main.tf
@@ -26,10 +26,20 @@ resource "aws_instance" "app_server" {
   provisioner "file" {
     source = "index.html"
     destination = "/tmp/index.html"
+    connection {
+        type = "ssh"
+        user = "ubuntu"
+        host =  self.public_ip
+        }
   
 }
 
   provisioner "local-exec" {
-    command = "sudo apt-get update && sudo apt-get -y install apache2 && sudo cp /tmp/index.html /var/www/html"
+    command = "sudo apt-get update && sudo apt-get -y install apache2 && sudo cp /tmp/index.html /var/www/html
+    connection {
+        type = "ssh"
+        user = "ubuntu"
+        host =  self.public_ip
+      }
   }
 }
