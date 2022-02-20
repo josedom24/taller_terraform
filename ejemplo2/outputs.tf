80f7a2d519dbfee7575fe38669aaaff5d5099f80
José Domingo Muñoz <josedom24@gmail.com>
2022-02-20 20:20:59 +0100

    terraform

--- ejemplo2/outputs.tf
+++ ejemplo2/outputs.tf
@@ -1,9 +1,7 @@
-output "instance_ip_addr" 
-{  
+output "instance_ip_addr" {  
     value = aws_instance.app_server.private_ip
 }
 
-output "instance_url" 
-{  
+output "instance_url" {  
     value = aws_instance.app_server.public_dns
 }
