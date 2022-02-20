ae3f2a9bed7d69bf728618b4d2463c8a91bec296
José Domingo Muñoz <josedom24@gmail.com>
2022-02-20 19:44:07 +0100

    terraform

--- ejemplo2/main.tf
+++ ejemplo2/main.tf
@@ -15,10 +15,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0892d3c7ee96c0bf7"
   instance_type = "t2.micro"
-
+  key_name = "key_jdmr"
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "InstanciaPrueba"
   }
+
+# Copies the file as the root user using SSH
+provisioner "file" {
+  source = "index.html"
+  destination = "/tmp/index.html"
+  connection {
+      type = "ssh"
+      user = "ubuntu"
+      }
+}
+# Ejecuta remotamente
+provisioner "remote-exec" {
+  connection {
+      type = "ssh"
+      user = "ubuntu"
+      }
+  inline = [
+    "sudo apt-get update",
+    "sudo apt-get -y install apache2",
+    "sudo cp /tmp/index.html /var/www/html",
+  ]
+}
 }
