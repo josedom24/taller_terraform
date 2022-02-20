ac70166c2027f34d5cfa167ff9e61dcc4a3e8d99
José Domingo Muñoz <josedom24@gmail.com>
2022-02-20 20:48:21 +0100

    terraform

--- ejemplo2/main.tf
+++ ejemplo2/main.tf
@@ -22,17 +22,18 @@ resource "aws_instance" "app_server" {
     Name = "InstanciaPrueba"
   }
 
-## Copies the file as the root user using SSH
-#  provisioner "file" {
-#    source = "index.html"
-#    destination = "/tmp/index.html"
-#    connection {
-#        type = "ssh"
-#        user = "ubuntu"
-#        host =  self.public_ip
-#        }
-#  
-#}
+# Copies the file as the root user using SSH
+  provisioner "file" {
+    source = "index.html"
+    destination = "/tmp/index.html"
+    connection {
+        type = "ssh"
+        user = "ubuntu"
+        agent= true
+        host =  self.public_ip
+        }
+  
+}
 
 #  provisioner "local-exec" {
 #    command = "sudo apt-get update && sudo apt-get -y install apache2 && sudo cp /tmp/index.html /var/www/html"
