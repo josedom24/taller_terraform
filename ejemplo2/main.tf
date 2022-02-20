6617ff4f3a4e2a7996399b01ed56aeaf57029d13
José Domingo Muñoz <josedom24@gmail.com>
2022-02-20 20:38:06 +0100

    terraform

--- ejemplo2/main.tf
+++ ejemplo2/main.tf
@@ -37,4 +37,4 @@ resource "aws_instance" "app_server" {
 #  provisioner "local-exec" {
 #    command = "sudo apt-get update && sudo apt-get -y install apache2 && sudo cp /tmp/index.html /var/www/html"
 #  }
-#}
+}
